a1a9a9c53b21b9b27580ce1d1e96d593e50bd6dc
Samuel Palacios <samuelpl0888@gmail.com>
2026-04-19 17:30:40 -0400

    fix: disable auto line wrapping in nvim comments

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.opt.textwidth = 110
-vim.opt.formatoptions = "tcqrn"
+vim.opt.textwidth = 0
+vim.opt.formatoptions = "qrn"
 
 require("config.lazy")
